45560f97a9e4f2fc6b35b61eb9655e5e9f6160bc
S David Kong <SDAVIDKONG@gmail.com>
2022-10-15 18:17:56 -0400

    yeah

--- contracts/customTokenOz.sol
+++ contracts/customTokenOz.sol
@@ -1,12 +1,10 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.7;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol"
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract CustomTokenOZ is ERC20 {
-
     constructor(uint256 initialSupply) ERC20("CustomTokenOZ", "CTOZ") {
-        _mint(msg.msg.sender, initialSupply);
+        _mint(msg.sender, initialSupply);
     }
-
 }
